a8ab3f5414ffea857a0fd894e0c582987a3f8382
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-03-02 19:35:32 +0530

    Spinner added

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,39 @@
+const spinner = (displayStyle) => {
+  document.getElementById("loading-gifs").style.display = displayStyle;
+};
+
 const allMobiles = () => {
   document.getElementById("product-zone").innerHTML = "";
-  document.getElementById("loading-gif").style.display = "block";
-  const searchBox = document.getElementById("search-box").value;
-  const mobileUrl = ` https://openapi.programming-hero.com/api/phones?search=${searchBox}`;
+  // spinner("block");
 
+  const searchBox = document.getElementById("search-box").value;
+  spinner("block");
+  const mobileUrl = ` https://openapi.programming-hero.com/api/phones?search=${searchBox}`;
   console.log(mobileUrl);
   fetch(mobileUrl)
     .then((response) => response.json())
     .then((data) => displayMobiles(data.data));
-  document.getElementById("loading-gif").style.display = "none";
+  // document.getElementById("loading-gif").style.display = "none";
 };
-
 const displayMobiles = (mobiles) => {
   const first20Data = mobiles.slice(0, 20);
-  for (const mobile of first20Data) {
-    const productZone = document.getElementById("product-zone");
-    const div = document.createElement("div");
-    div.innerHTML = ` <div class="col">
+
+  if (mobiles.length == "") {
+    Swal.fire({
+      title:
+        "Your Searched Product is Not Found \n Product Available : OPPO, SAMSUNG, IPHONE, HUAWEI",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  } else {
+    for (const mobile of first20Data) {
+      const productZone = document.getElementById("product-zone");
+      const div = document.createElement("div");
+      div.innerHTML = ` <div class="col">
     <div class="card p-3">
         <img src="${mobile.image}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -26,11 +43,12 @@ const displayMobiles = (mobiles) => {
         </div>
     </div>
 </div>`;
-    productZone.appendChild(div);
-    console.log(mobile);
+      productZone.appendChild(div);
+      console.log(mobile);
+    }
+    spinner("none");
   }
 };
-
 const seeMore = (info) => {
   const mobileUrl = `https://openapi.programming-hero.com/api/phone/${info}`;
   fetch(mobileUrl)
@@ -38,7 +56,6 @@ const seeMore = (info) => {
     .then((data) => setDetails(data.data));
   window.scrollTo(0, 0);
 };
-
 const setDetails = (info) => {
   document.getElementById("mobile-fetchers").innerHTML = `
   <div class="col-0 col-sm-4"></div>
@@ -56,7 +73,6 @@ const setDetails = (info) => {
       <p>Storage : ${info.mainFeatures.storage}</p>
   </div>
 </div>`;
-
   if (info.mainFeatures.status == true) {
     document.getElementById("error-massage").style.display = "block";
   }
